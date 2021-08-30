b5ed6b2890cdd8bbca4bfb039c2e0e6fad06160b
Ben Croker <ben@putyourlightson.com>
2021-08-30 20:01:05 +0200

    Specify major versions for libraries

--- src/assets/ReportsAsset.php
+++ src/assets/ReportsAsset.php
@@ -26,14 +26,14 @@ class ReportsAsset extends AssetBundle
 
         // define the relative path to CSS/JS files that should be registered with the page when this asset bundle is registered
         $this->css = [
-            'https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css',
+            'https://cdn.jsdelivr.net/npm/daterangepicker@3/daterangepicker.css',
             'https://cdn.datatables.net/v/dt/dt-1.11.0/r-2.2.9/datatables.min.css',
             'css/cp.css',
         ];
         $this->js = [
-            'https://cdn.jsdelivr.net/npm/apexcharts/dist/apexcharts.min.js',
-            'https://cdn.jsdelivr.net/npm/moment/moment.min.js',
-            'https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js',
+            'https://cdn.jsdelivr.net/npm/apexcharts@3/dist/apexcharts.min.js',
+            'https://cdn.jsdelivr.net/npm/moment@2/moment.min.js',
+            'https://cdn.jsdelivr.net/npm/daterangepicker@3/daterangepicker.min.js',
             'https://cdn.datatables.net/v/dt/dt-1.11.0/r-2.2.9/datatables.min.js',
         ];
 
