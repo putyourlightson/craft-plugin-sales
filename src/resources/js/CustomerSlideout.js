acf3cd6a9a4f164cbd0ec1f0dd42dac6e21c2a9b
bencroker <ben@putyourlightson.com>
2023-12-04 19:25:29 -0600

    JS refactoring

--- src/resources/js/CustomerSlideout.js
+++ src/resources/js/CustomerSlideout.js
@@ -4,17 +4,16 @@ PluginSales.CustomerSlideout = Craft.CpScreenSlideout.extend(
     {
         init: function(customer) {
             const action = 'plugin-sales/slideout/render?customer=' + encodeURIComponent(customer);
-            this.base(action);
-
-            this.open();
-            this.$saveBtn.remove();
-            this.$cancelBtn.text(Craft.t('app', 'Close'));
 
             this.on('load', () => {
                 this.removeNamespace();
-                this.removeFocus();
-                htmx.process(this.$container[0]);
+                this.processHtmx();
             });
+
+            this.base(action);
+
+            this.$saveBtn.remove();
+            this.$cancelBtn.text(Craft.t('app', 'Close'));
         },
 
         removeNamespace: function() {
@@ -28,17 +27,8 @@ PluginSales.CustomerSlideout = Craft.CpScreenSlideout.extend(
             });
         },
 
-        removeFocus: function() {
-            // Defer removing the focus for at least two timeouts, to be sure!
-            setTimeout(() => {
-                document.activeElement.blur();
-            }, 100);
-            setTimeout(() => {
-                document.activeElement.blur();
-            }, 300);
-            setTimeout(() => {
-                document.activeElement.blur();
-            }, 500);
+        processHtmx: function() {
+            htmx.process(this.$container[0]);
         },
 
         handleSubmit: function(event) {
