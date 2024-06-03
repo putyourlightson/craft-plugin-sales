4612f12f2f501b2467892d05e71e415c7ce01857
bencroker <ben@putyourlightson.com>
2024-06-03 21:58:44 +0200

    Improve queries

--- src/services/PluginsService.php
+++ src/services/PluginsService.php
@@ -49,7 +49,7 @@ class PluginsService extends Component
     public function getNames(): array
     {
         return PluginRecord::find()
-            ->select('name')
+            ->select(['name'])
             ->column();
     }
 
@@ -59,7 +59,7 @@ class PluginsService extends Component
     public function create(int $id, string $name, bool $hasMultipleEditions): void
     {
         if ($this->_pluginIds === null) {
-            $this->_pluginIds = PluginRecord::find()->select('id')->column();
+            $this->_pluginIds = PluginRecord::find()->select(['id'])->column();
         }
 
         if (in_array($id, $this->_pluginIds)) {
